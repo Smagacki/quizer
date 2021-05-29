7cde6620721df0709a199a9c34cfa8191753959a
wsmagacki <waldemar.smagacki@gmail.com>
2021-05-30 00:58:46 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/jwd/quizer/QuizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.quizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
