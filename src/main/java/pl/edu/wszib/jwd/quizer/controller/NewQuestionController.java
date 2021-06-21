31e97e8d6d81750a4c857d72a66aeb7252ea956f
wsmagacki <waldemar.smagacki@gmail.com>
2021-06-22 00:09:57 +0200

    Poprawa funkcjonalnosci zapisu nowych pytan

--- src/main/java/pl/edu/wszib/jwd/quizer/controller/NewQuestionController.java
+++ src/main/java/pl/edu/wszib/jwd/quizer/controller/NewQuestionController.java
@@ -11,6 +11,8 @@ import pl.edu.wszib.jwd.quizer.dao.QuestionDao;
 import pl.edu.wszib.jwd.quizer.model.Answer;
 import pl.edu.wszib.jwd.quizer.model.Question;
 
+import java.util.*;
+
 @Controller
 public class NewQuestionController {
     @Autowired
@@ -27,14 +29,44 @@ public class NewQuestionController {
 
     @PostMapping("/add-question")
     public String addQuestion(@ModelAttribute Question question) {
+        question.getAnswers().get(0).setCorrect(true);
+        List<Answer> userAnswers = new ArrayList<>();
+        for (Answer a : question.getAnswers()) {
+            if (a.getAnswerText() != null && !a.getAnswerText().isEmpty())
+                userAnswers.add(a);
+            else
+                break;
+        }
+        question.setAnswers(getMixedAnswersList(userAnswers));
+
         questionDao.save(question);
+
         for (int i = 0; i < question.getAnswers().size(); i++) {
             String answerText = question.getAnswers().get(i).getAnswerText();
-            if ((answerText != null) && !answerText.isEmpty())
-                answerDao.save(new Answer(question, i + 1, answerText, false));
+            boolean isCorrect = question.getAnswers().get(i).isCorrect();
+            if (answerText != null && !answerText.isEmpty())
+                answerDao.save(new Answer(question, i + 1, answerText, isCorrect));
             else
                 break;
         }
         return "redirect:/new_question";
+    }
+
+    private List<Answer> getMixedAnswersList(List<Answer> answers) {
+        Random rd = new Random();
+
+        List<Integer> randomInt = new ArrayList<>();
+        while (randomInt.size() < answers.size()) {
+            int x = rd.nextInt(answers.size());
+            if (!randomInt.contains(x))
+                randomInt.add(x);
+        }
+
+        List<Answer> mixedList = new ArrayList<>();
+        for (int i = 0; i < answers.size(); i++) {
+            mixedList.add(answers.get(randomInt.get(i)));
+        }
+
+        return mixedList;
     }
 }
